10a26155aa19314b2a2d09b7cde1119728aacdba
Marta Lis <marta.lis1501@gmail.com>
2023-01-03 12:23:59 +0100

    Santas's List Children

---
+++ Santa's_Children.py
@@ -0,0 +1,14 @@
+
+name=(input("Podaj imie dziecka: "))
+print("Cześć Mikołaju, dziecko o imieniu: ", name, "było")
+if name == "Marta Lis":
+    print("było grzeczne. ")
+elif name == "Magda Pieczarka":
+    print("było grzeczne. ")
+elif name == "Anna Lewandowska":
+    print("było grzeczne. ")
+elif name == "Monika Ozga":
+    print("było grzeczne. ")
+else :
+    print("było bardzo niegrzeczne.")
+    
